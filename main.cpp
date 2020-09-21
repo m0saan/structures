ca2756cbbbd1ec422db9abc383c2ab2a8f7facad
MoBoustta <moboustta6@gmail.com>
2020-09-21 13:55:55 +0100

    __CPP__ Data__Structures(Templated HashTable been implemented)

--- main.cpp
+++ main.cpp
@@ -1,13 +1,23 @@
 #include <iostream>
 #include <string>
-#include "Exercises.h"
+#include "Map.h"
+#include "Map.cpp"
+#include <map>
 
 
 int main(){
-    std::string str{ "gren apl" };
+    std::map<int, std::string> map1;
+    map1.insert({6, "A"});
+    map1.insert({8, "B"});
+    map1.insert({11, "C"});
+    map1.insert({6, "A+"});
+    Map<std::string , std::string> map(10);
+    map.put("6", "A");
+    map.put("8", "B");
+    map.put("11", "C");
+    map.put("6", "A+");
 
-    std::cout << Exercises::findFirstRepeatedCharacter(str) << std::endl;
+    std::cout << map.get("11") << std::endl;
     return 0;
 }
 
-// [ 10, 20]
