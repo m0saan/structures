50d95106ac1fce6b8f6ea58e9fbb4910dd6c5187
MoBoustta <moboustta6@gmail.com>
2020-09-21 13:54:58 +0100

    __CPP__ Data__Structures(Ex)

--- Map.h
+++ Map.h
@@ -10,53 +10,71 @@
 #include <utility>
 #include <vector>
 #include <iostream>
+#include <cstring>
 
+template<typename MAP>
 class KVPair {
 public:
-    KVPair(int k, std::string v) : key{k}, value{std::move(v)} {}
+
+    using KeyType = typename MAP::KeyType;
+    using ValueType = typename MAP::ValueType;
+public:
+    KVPair(KeyType k, ValueType v) : key{ k }, value{ v } {}
     bool operator==(const KVPair& rhs) { return key = rhs.key && value == rhs.value; }
 
-    int key;
-    std::string value;
+    KeyType key;
+    ValueType value;
 };
 
+template<typename KEY, typename VALUE>
 class Map {
 public:
-    explicit Map(std::size_t initial_capacity) : init_capacity{initial_capacity} {
-        table = new std::vector<std::list<KVPair>>(initial_capacity, std::list<KVPair>{});
+    using KeyType = KEY;
+    using ValueType = VALUE;
+    using KVPAIR = KVPair<Map<KEY, VALUE>>;
+public:
+    explicit Map(std::size_t initial_capacity)  {
+        table = new std::vector<std::list<KVPAIR>>(initial_capacity, std::list<KVPAIR>{});
         //buckets->resize(initial_capacity);
     };
 
-    void put(int k, const std::string &v) {
-        if (!(isValidKey(k))) throw std::runtime_error{"Invalid key"};
+    void put(KEY k, const VALUE v) {
         std::size_t index = hash(k);
         for (const auto &iter : table->at(index))
             if (iter.key == k) return;
-        table->at(index).push_back(KVPair{k, v});
+        table->at(index).push_back(KVPAIR { k, v });
     }
 
-    std::string get(int k) {
-        if (!(isValidKey(k))) throw std::runtime_error{"Invalid key"};
+    std::string get(KEY k) {
         return getBucket(k).value;
-        throw std::logic_error{ "No such element" };
     }
 
-    void remove(int k){
-        if (!(isValidKey(k))) throw std::runtime_error{"Invalid key"};
+    void remove(KEY k){
         auto bucket = getBucket(k);
         table->at(hash(k)).remove(bucket);
     }
 
 private:
-    std::size_t init_capacity;
-    std::vector<std::list<KVPair>> *table;
+    std::vector<std::list<KVPAIR>> *table;
 private:
-    std::size_t hash(int key) const { return key % table->capacity(); }
-    static bool isValidKey(int &k) { return k >= 0; }
-    KVPair getBucket(int &k){
+    std::size_t sToi(KEY k){
+        std::size_t out = 0;
+        for (int i = 0; i < strlen(k); ++i)
+            out += k[i];
+        return out;
+    }
+
+    std::size_t hash(KEY key) const {
+        std::hash<KEY> hash;
+        return hash(key) % table->capacity();
+    }
+
+    static bool isValidKey(std::size_t &k) { return k >= 0; }
+
+    KVPAIR getBucket(KEY &k) {
         auto bucket = table->at(hash(k));
         for (auto iterator = bucket.cbegin(); iterator != bucket.end(); ++iterator)
-            if (iterator->key == k) return KVPair{ k, iterator->value };
+            if (iterator->key == k) return KVPAIR { k, iterator->value };
         throw std::logic_error{ "No such element" };
     }
 };
