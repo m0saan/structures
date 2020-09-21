7c4a04f1523be12de20ad6ebce0e3dfa208017c2
MoBoustta <moboustta6@gmail.com>
2020-09-21 14:38:39 +0100

    __CPP__ Data__Structures(Simple Templated HashTable been implemented)

--- Map.cpp
+++ Map.cpp
@@ -3,3 +3,56 @@
 //
 
 #include "Map.h"
+
+template<typename KEY, typename VALUE>
+Map<KEY, VALUE>::Map(std::size_t initial_capacity)  {
+    table = new std::vector<std::list<KVPAIR>>(initial_capacity, std::list<KVPAIR>{});
+}
+
+template<typename KEY, typename VALUE>
+Map<KEY, VALUE>::~Map() {
+    delete table;
+    table = nullptr;
+}
+
+template<typename KEY, typename VALUE>
+void Map<KEY, VALUE>::put(const KEY k, const VALUE v) {
+    std::size_t index = hash(k);
+    for (const auto &iter : table->at(index))
+        if (iter.key == k) return;
+    table->at(index).push_back(KVPAIR { k, v });
+}
+
+template<typename KEY, typename VALUE>
+std::string Map<KEY, VALUE>::get(KEY k) {
+    return getBucket(k).value;
+}
+
+template<typename KEY, typename VALUE>
+void Map<KEY, VALUE>::remove(KEY k) {
+    auto bucket = getBucket(k);
+    table->at(hash(k)).remove(bucket);
+}
+
+template<typename KEY, typename VALUE>
+std::size_t Map<KEY, VALUE>::hash(KEY key) const {
+    std::hash<KEY> hash;
+    return hash(key) % table->capacity();
+}
+
+template<typename KEY, typename VALUE>
+bool Map<KEY, VALUE>::isValidIndex(size_t &k) { return k >= 0; }
+
+template<typename KEY, typename VALUE>
+typename Map<KEY, VALUE>::KVPAIR Map<KEY, VALUE>::getBucket(KEY &k) {
+    auto bucket = table->at(hash(k));
+    for (auto iterator = bucket.cbegin(); iterator != bucket.end(); ++iterator)
+        if (iterator->key == k) return KVPAIR { k, iterator->value };
+    throw std::logic_error{ "No such element" };
+}
+
+template<typename MAP>
+KVPair<MAP>::KVPair(KeyType k, ValueType v) : key{ k }, value{ v } {}
+
+template<typename MAP>
+bool KVPair<MAP>::operator==(const KVPair &rhs) const { return key == rhs.key && value == rhs.value; }
--- Map.h
+++ Map.h
@@ -15,12 +15,11 @@
 template<typename MAP>
 class KVPair {
 public:
-
     using KeyType = typename MAP::KeyType;
     using ValueType = typename MAP::ValueType;
 public:
-    KVPair(KeyType k, ValueType v) : key{ k }, value{ v } {}
-    bool operator==(const KVPair& rhs) { return key = rhs.key && value == rhs.value; }
+    KVPair(KeyType k, ValueType v);
+    bool operator==(const KVPair& rhs) const;
 
     KeyType key;
     ValueType value;
@@ -33,51 +32,20 @@ public:
     using ValueType = VALUE;
     using KVPAIR = KVPair<Map<KEY, VALUE>>;
 public:
-    explicit Map(std::size_t initial_capacity)  {
-        table = new std::vector<std::list<KVPAIR>>(initial_capacity, std::list<KVPAIR>{});
-        //buckets->resize(initial_capacity);
-    };
-
-    void put(KEY k, const VALUE v) {
-        std::size_t index = hash(k);
-        for (const auto &iter : table->at(index))
-            if (iter.key == k) return;
-        table->at(index).push_back(KVPAIR { k, v });
-    }
-
-    std::string get(KEY k) {
-        return getBucket(k).value;
-    }
-
-    void remove(KEY k){
-        auto bucket = getBucket(k);
-        table->at(hash(k)).remove(bucket);
-    }
+    explicit Map(std::size_t initial_capacity=5);
+    ~Map();
+    void put(KEY k, VALUE v);
+    std::string get(KEY k);
+    void remove(KEY k);
 
 private:
     std::vector<std::list<KVPAIR>> *table;
 private:
-    std::size_t sToi(KEY k){
-        std::size_t out = 0;
-        for (int i = 0; i < strlen(k); ++i)
-            out += k[i];
-        return out;
-    }
-
-    std::size_t hash(KEY key) const {
-        std::hash<KEY> hash;
-        return hash(key) % table->capacity();
-    }
-
-    static bool isValidKey(std::size_t &k) { return k >= 0; }
-
-    KVPAIR getBucket(KEY &k) {
-        auto bucket = table->at(hash(k));
-        for (auto iterator = bucket.cbegin(); iterator != bucket.end(); ++iterator)
-            if (iterator->key == k) return KVPAIR { k, iterator->value };
-        throw std::logic_error{ "No such element" };
-    }
+    std::size_t hash(KEY key) const;
+    static bool isValidIndex(std::size_t &index);
+    KVPAIR getBucket(KEY &k);
 };
+
 
 
 #endif //DATA_STRUCTURES_AND_ALGORITHMS_MAP_H
