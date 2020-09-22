85552a7dcb5cae19e212bdff54d55f18f34e0103
MoBoustta <moboustta6@gmail.com>
2020-09-22 22:14:13 +0100

    __CPP__ Data__Structures Simple Map implemented using linear probing method

--- MapWithLinearProbing.h
+++ MapWithLinearProbing.h
@@ -2,42 +2,66 @@
 #define MAP_WITH_LINEAR_PROBING_H
 
 #include<string>
+#include <utility>
 
 class KVPair {
-  public:
-    KVPair(int k, std::string& v) : key { k }, value { v } {}
-  private:
-      int key;
-      std::string value;
+public:
+    KVPair() = default;
+
+    KVPair(int k, std::string v) : key{k}, value{std::move(v)} {}
+
+    int key{};
+    std::string value;
 };
 
 class MapWithLinearProbing {
-  public:
-  MapWithLinearProbing(std::size_t capacity) : capacity { capacity } {
-    entries = new KVPair[capacity]{};
-  };
+public:
+    explicit MapWithLinearProbing(std::size_t capacity) : capacity{capacity} {
+        entries = new KVPair[capacity]{};
+        tracker = new bool[capacity]{false};
+    };
 
-  ~MapWithLinearProbing() { delete entries; }
+    ~MapWithLinearProbing() { delete entries; }
 
-  void put(int k, std::string& value){
-    KVPair kvPair{k, v};
-    std::size_t index = hash(k);
-    if (entries[index] == nullptr)
-      entries[index] = kvPair;
-    else{
-      int i {};
-      while(i++){
-        if (entries[index + i] == nullptr)
-          entries[index + i] = kvPair;
-        return;
-      }
+    void put(int k, const std::string &v) {
+        std::size_t index = hash(k);
+        if (tracker[index] == false) insertKvPair(k, v, index);
+        else findSlotAndInsertKVPair(k, v, index);
     }
-  }
 
-  private:
-  std::size_t capacity;
-  KVPair *entries;
+    void print() const {
+        for (int i = 0; i < capacity; ++i) {
+            if (tracker[i] != false)
+                std::cout << "(" << entries[i].key << ", " << entries[i].value << ")" << std::endl;
+        }
+    }
 
-  private:
-  std::size_t hash(int &k) { return k % capacity; }
+
+private:
+
+    std::size_t capacity;
+    KVPair *entries;
+    bool *tracker;
+
+private:
+
+    std::size_t hash(int &k) const { return k % capacity; }
+
+    void insertKvPair(int &k, const std::string &v, const std::size_t &index) {
+        KVPair kvPair{k, v};
+        entries[index] = kvPair;
+        tracker[index] = true;
+    }
+
+    void findSlotAndInsertKVPair(int &k, const std::string &v, std::size_t &index) {
+        int i{};
+        while (++i) {
+            if (tracker[index + i] == false) {
+                insertKvPair(k, v, index + i);
+                return;
+            }
+        }
+    }
 };
+
+#endif
