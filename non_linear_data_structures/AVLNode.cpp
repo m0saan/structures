5b1b50932bf7c289f6362716b84163e65bd1399a
MoBoustta <moboustta6@gmail.com>
2020-10-03 16:56:34 +0100

    Add Header to srcs

--- non_linear_data_structures/AVLNode.cpp
+++ non_linear_data_structures/AVLNode.cpp
@@ -1,6 +1,24 @@
-//
-// Created by moboustt on 10/1/20.
-//
+/*******************************************************************************
+ * DATA STRUCTURES IMPLEMENTATIONS
+ *
+ *   __                __
+ *  |  \  _  |_  _    (_  |_  _      _ |_      _  _  _
+ *  |__/ (_| |_ (_|   __) |_ |  |_| (_ |_ |_| |  (- _)
+ *
+ *
+ * Adelson-Velskii and Landis' (AVL) tree
+ *
+ * Features, being N the number of elements in the tree:
+ * 1. Guaranteed search time is O(log(N)).
+ * 2. Dynamically updated/balanced tree structure O(N) storage.
+ *
+ * http://en.wikipedia.org/wiki/AVL_tree
+ *
+ * @author (moboustta6@gmail.com)
+ * @github MoBoustta
+ *
+ ******************************************************************************/
+
 
 #include "AVLNode.h"
 
