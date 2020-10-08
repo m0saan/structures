1de5880c03f401d8787355d7d83ebddce2257236
MoBoustta <moboustta6@gmail.com>
2020-10-08 21:57:59 +0100

    Move the Node class to a seprate file

--- non_linear_data_structures/Node.cpp
+++ non_linear_data_structures/Node.cpp
@@ -3,3 +3,18 @@
 //
 
 #include "Node.h"
+
+template<typename GRAPH>
+Node<GRAPH>::Node(const T& label) : label { label } {};
+
+
+template<typename GRAPH>
+void Node<GRAPH>::addEdge(Node *to, int weight) {
+    edges.push_back(new Edge{ this, to, weight});
+}
+
+
+template<typename GRAPH>
+std::vector<typename Node<GRAPH>::Edge*> &Node<GRAPH>::getEdges() {
+    return edges;
+}
