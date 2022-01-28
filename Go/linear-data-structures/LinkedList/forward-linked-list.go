78255a98d6fd35f926290b2e7718414d6605e7b8
M0-san <moboustta6@gmail.com>
2022-01-28 22:32:40 +0100

    create a LinkedList README and start implementing it

---
+++ Go/linear-data-structures/LinkedList/forward-linked-list.go
@@ -0,0 +1,22 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+* DATA STRUCTURES IMPLEMENTATIONS													//
+*																					//
+*   __                __															//
+*  |  \  _  |_  _    (_  |_  _      _ |_      _  _  _								//
+*  |__/ (_| |_ (_|   __) |_ |  |_| (_ |_ |_| |  (- _)								//
+*																					//
+*																					//
+* Linked List -> Forward Liked List.												//
+*																					//
+* Features, being n the number of elements in the tree:							//
+* 1. Look-ups 		-> O(n)															//
+* 2. Insertions:
+		-> O(n); O(1) on average										//
+* 3. Deletions  	-> O(n)															//
+*																					//
+* http://en.wikipedia.org/wiki/(should be updated)									//
+*																					//
+* @author (moboustta6@gmail.com)													//
+* @github M0-san																	//
+*																					//
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
---
+++ Go/linear-data-structures/LinkedList/linkedList.md
@@ -0,0 +1,10 @@
+
+### Linked List
+    We use a linkedlist to store objects in a squential manner in memory,       
+    But unlike arrays, likendlists can grow and shrink automatically.
+    
+    /*     ---------    ---------           */
+    /*     | Value |    | Value |           */
+    /*     |  ---  |    |  ---  |           */
+    /*     |  next-|--> |  next-|--> NULL   */
+    /*     ---------    ---------           */
