56c31207e91cc3c8acded8bde889344ded74a367
M0-san <moboustta6@gmail.com>
2022-01-28 23:03:08 +0100

    design the interface and the structues

--- Go/linear-data-structures/LinkedList/forward-linked-list.go
+++ Go/linear-data-structures/LinkedList/forward-linked-list.go
@@ -8,15 +8,50 @@
 *																					//
 * Linked List -> Forward Liked List.												//
 *																					//
-* Features, being n the number of elements in the tree:							//
+* Features, being n the number of elements in the tree:								//
 * 1. Look-ups 		-> O(n)															//
-* 2. Insertions:
-		-> O(n); O(1) on average										//
-* 3. Deletions  	-> O(n)															//
+* 2. Insertions:    -> O(n); O(1) at Front/Back										//
+* 3. Deletions  	-> O(n); O(1) at Front/Back										//
 *																					//
-* http://en.wikipedia.org/wiki/(should be updated)									//
+* https://en.wikipedia.org/wiki/Linked_list											//
 *																					//
 * @author (moboustta6@gmail.com)													//
 * @github M0-san																	//
 *																					//
 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+package main
+
+type Node struct {
+	value int
+	next  *Node
+}
+
+type ForwardList struct {
+	size       int
+	head, tail *Node
+}
+
+type IForawrdList interface {
+	// Element Access
+	front() int
+	back() int
+
+	// Modifiers
+	pushBack(elem int) int
+	pushFront(elem int) int
+	popBack() int
+	popFront() int
+	splice(elem int)
+	clear()
+
+	// Capacity
+	isEmpty() bool
+	length() int
+
+	// Operations
+	contains(elem int) bool
+	indexOf(elem int) int
+	reverse()
+	sort()
+}
