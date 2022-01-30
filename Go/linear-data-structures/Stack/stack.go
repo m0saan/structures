18c792e5972b40f685ca867e55cdd55c07736380
M0-san <moboustta6@gmail.com>
2022-01-30 23:25:36 +0100

    implemening the stack -> ok!

--- Go/linear-data-structures/Stack/stack.go
+++ Go/linear-data-structures/Stack/stack.go
@@ -21,3 +21,50 @@
 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 package main
+
+import "fmt"
+
+type Stack struct {
+	vals []interface{}
+}
+
+func (s Stack) String() string {
+	return fmt.Sprintf("%v", s.vals)
+}
+
+func (s *Stack) isEmpty() bool {
+	return len(s.vals) == 0
+}
+
+func (s *Stack) Push(val interface{}) {
+	s.vals = append(s.vals, val)
+}
+
+func (s *Stack) Pop() (interface{}, bool) {
+	if s.isEmpty() {
+		return nil, false
+	}
+	ret := s.vals[len(s.vals)-1]
+	s.vals = s.vals[:len(s.vals)-1]
+	return ret, false
+}
+
+func (s *Stack) Peek() (interface{}, bool) {
+	if s.isEmpty() {
+		return nil, false
+	}
+	return s.vals[len(s.vals)-1], true
+}
+
+func main() {
+	s := Stack{}
+	s.Push(10)
+	s.Push(20)
+	s.Push(30)
+	s.Push(40)
+
+	for s.isEmpty() != true {
+		v, _ := s.Pop()
+		fmt.Println(v)
+	}
+}
